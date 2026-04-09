3af33c33b43a1ee748a6a199ab52e0127d34a2b2
Hamid Raza <shaikh.hamidraza.ali786@gmail.com>
2026-04-10 01:14:10 +0530

    add app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load model
+model = joblib.load("housing_model.pkl")
+
+st.title("California Housing Price Prediction")
+
+st.write("Enter housing features to predict house price")
+
+MedInc = st.number_input("Median Income")
+HouseAge = st.number_input("House Age")
+AveRooms = st.number_input("Average Rooms")
+AveBedrms = st.number_input("Average Bedrooms")
+Population = st.number_input("Population")
+AveOccup = st.number_input("Average Occupancy")
+Latitude = st.number_input("Latitude")
+Longitude = st.number_input("Longitude")
+
+if st.button("Predict Price"):
+
+    features = np.array([[MedInc, HouseAge, AveRooms, AveBedrms,
+                          Population, AveOccup, Latitude, Longitude]])
+
+    prediction = model.predict(features)
+
+    st.success(f"Predicted House Price: {prediction[0]:.2f}")
